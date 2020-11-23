9f22b6092eb1c89818336f56594f52c5f3d3171e
Resul134 <resulkurrr134@gmail.com>
2020-11-23 14:06:54 +0100

    Revert "Ændring på navngivning af projektet"
    This reverts commit 73a483125e446d2c4a415fcd9869fcea7483e58c.

--- HOVEDOPGAVE/Startup.cs
+++ HOVEDOPGAVE/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace RestAPI
+namespace HOVEDOPGAVE
 {
     public class Startup
     {
