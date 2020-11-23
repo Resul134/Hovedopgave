73a483125e446d2c4a415fcd9869fcea7483e58c
Resul134 <resulkurrr134@gmail.com>
2020-11-23 14:06:43 +0100

    Ændring på navngivning af projektet

--- HOVEDOPGAVE/Program.cs
+++ HOVEDOPGAVE/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace HOVEDOPGAVE
+namespace RestAPI
 {
     public class Program
     {
