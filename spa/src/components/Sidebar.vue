986fd431c045d70dcb61830b017537d02d4e9f8c
Simon Nielsen <43374884+GretheSlushice@users.noreply.github.com>
2020-11-27 17:18:36 +0100

    Updated sidebar
    -Mere dynamisk indlæsning
    -Design ændringer

--- spa/src/components/Sidebar.vue
+++ spa/src/components/Sidebar.vue
@@ -1,19 +1,17 @@
 <template>
 <div class="root">
-    <div class="px-3 py-2">
+    <div>
         <h2>Kategorier</h2>
-        <b-list-group>
-            <b-list-group-item v-on:click="filterCategory('1')" href="" v-bind:active="activeOne">Kategori 1</b-list-group-item>
-            <b-list-group-item v-on:click="filterCategory('2')" href="" v-bind:active="activeTwo">Kategori 2</b-list-group-item>
-            <b-list-group-item v-on:click="filterCategory('3')" href="" v-bind:active="activeThree">Kategori 3</b-list-group-item>
-            <b-list-group-item v-on:click="filterCategory('4')" href="" v-bind:active="activeFour">Kategori 4</b-list-group-item>
-            <b-list-group-item v-on:click="filterCategory('5')" href="" v-bind:active="activeFive">Kategori 5</b-list-group-item>
+        <b-list-group id="group">
+            <b-list-group-item id="item" class="hvr-sweep-to-right" v-for="category in categories" :key="category" @click="filterCategory(category)" href="" v-bind:active="category.active">
+                {{ category.name }}
+            </b-list-group-item>
         </b-list-group>
-        <h2>Region</h2>
+        <h2 class="region">Region</h2>
         <b-list-group>
-            <b-list-group-item v-on:click="filterRegion('1')" href="" v-bind:active="activeRegionOne">Sjælland</b-list-group-item>
-            <b-list-group-item v-on:click="filterRegion('2')" href="" v-bind:active="activeRegionTwo">Jylland</b-list-group-item>
-            <b-list-group-item v-on:click="filterRegion('3')" href="" v-bind:active="activeRegionThree">Fyn</b-list-group-item>
+            <b-list-group-item id="item" class="hvr-sweep-to-right" v-for="region in regions" :key="region" @click="filterRegion(region)" href="" v-bind:active="region.active">
+                {{ region.name }}
+            </b-list-group-item>
         </b-list-group>
     </div>
 </div>
@@ -24,64 +22,39 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Sidebar extends Vue {
-    activeOne = false;
-    activeTwo = false;
-    activeThree = false;
-    activeFour = false;
-    activeFive = false;
+    categories = [
+        { id: 1, name: "Udendørs", active: false },
+        { id: 2, name: "IT", active: false },
+        { id: 3, name: "Korrektur", active: false },
+        { id: 4, name: "Gaming", active: false },
+        { id: 5, name: "Frivilig", active: false },
+        { id: 6, name: "Skolehjælp", active: false }]
 
-    activeRegionOne = false;
-    activeRegionTwo = false;
-    activeRegionThree = false;
+    currentCategory = this.categories[0];
 
-    setCategoryActive() {
-        this.activeOne = false;
-        this.activeTwo = false;
-        this.activeThree = false;
-        this.activeFour = false;
-        this.activeFive = false;
+    regions = [
+        { id: 1, name: "Sjælland", active: false },
+        { id: 2, name: "Jylland", active: false },
+        { id: 3, name: "Fyn", active: false }]
+
+    currentRegion = this.regions[0];
+
+    filterCategory(category: any) {
+        this.currentCategory.active = false;
+
+        category.active = true;
+        this.currentCategory = category;
+
+        //  Filtering categories needs to be added here
     }
 
-    setRegionActive() {
-        this.activeRegionOne = false;
-        this.activeRegionTwo = false;
-        this.activeRegionThree = false;
-    }
+    filterRegion(region: any) {
+        this.currentRegion.active = false;
 
-    filterCategory(category: string) {
-        this.setCategoryActive();
-        switch (category) {
-        case "1":
-            this.activeOne = true;
-            break;
-        case "2":
-            this.activeTwo = true;
-            break;
-        case "3":
-            this.activeThree = true;
-            break;
-        case "4":
-            this.activeFour = true;
-            break;
-        case "5":
-            this.activeFive = true;
-            break;
-        }
-    }
+        region.active = true;
+        this.currentRegion = region;
 
-    filterRegion(region: string) {
-        this.setRegionActive();
-        switch (region) {
-        case "1":
-            this.activeRegionOne = true;
-            break;
-        case "2":
-            this.activeRegionTwo = true;
-            break;
-        case "3":
-            this.activeRegionThree = true;
-            break;
-        }
+        //  Filtering regions needs to be added here
     }
 }
 </script>
@@ -90,16 +63,68 @@ export default class Sidebar extends Vue {
 @import "../assets/main.scss";
 
 h2 {
-    margin: 10px 0 10px 0;
+    margin: 10px 0 10px 5px;
+}
+.region {
+    margin: 40px 0 10px 5px;
 }
 
 .root {
-    background: $light;
+    background: white;
     position: fixed;
     top: 56px;
     width: 280px;
     height: 100%;
-    box-shadow: whitesmoke;
+    box-shadow: 5px 0px 5px whitesmoke;
 }
 
+#group {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+
+#item {
+    border-radius: 0;
+}
+/* Sweep To Right */
+.hvr-sweep-to-right {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  position: relative;
+  -webkit-transition-property: color;
+  transition-property: color;
+  -webkit-transition-duration: 0.2s;
+  transition-duration: 0.2s;
+}
+.hvr-sweep-to-right:before {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: $primary;
+  -webkit-transform: scaleX(0);
+  transform: scaleX(0);
+  -webkit-transform-origin: 0 50%;
+  transform-origin: 0 50%;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  -webkit-transition-duration: 0.2s;
+  transition-duration: 0.2s;
+  -webkit-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+}
+.hvr-sweep-to-right:hover, .hvr-sweep-to-right:focus, .hvr-sweep-to-right:active {
+  color: white;
+}
+.hvr-sweep-to-right:hover:before, .hvr-sweep-to-right:focus:before, .hvr-sweep-to-right:active:before {
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+}
 </style>
