2c6a64d43b3db686321494d78d664609d20f305e
Alexander Stenbøg <ast@visiosign.com>
2020-11-27 10:47:20 +0100

    Colors

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -23,24 +23,13 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./assets/main.scss";
+
+*, *:before, *:after {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1,h2,h3,h4,h5,h6,p,span,div {
+  color: $gray;
 }
 </style>
