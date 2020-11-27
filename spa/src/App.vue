433ae9aafa065992972632fc65d9e1011238fa38
Resul134 <43374840+Resul134@users.noreply.github.com>
2020-11-27 13:43:49 +0100

    Fixes and refactoring of TopHeader routing

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -4,7 +4,6 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/register">Register</router-link>
     </div>
     <div class="router-view">
       <router-view/>
