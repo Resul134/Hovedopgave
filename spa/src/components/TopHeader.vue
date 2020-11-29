82e2ac32ae58074e595af09c7da15c856a3fd30e
Resul134 <43374840+Resul134@users.noreply.github.com>
2020-11-29 18:35:04 +0100

    Tilføjet svg ikoner

--- spa/src/components/TopHeader.vue
+++ spa/src/components/TopHeader.vue
@@ -11,19 +11,28 @@
       <div class="login-div">
         <div v-if="loggedIn">
           <router-link to="/">
-            <b-img src="../assets/profileicon.png"></b-img>
+          <!-- icon for profile -->
+         <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-person-circle" fill="white" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+          <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+          <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+        </svg>
           </router-link>
         </div>
         <div v-else>
           <router-link to="/">
-             <b-img src="../assets/login.png"></b-img>
+          <!-- icon for log in -->
+          <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-box-arrow-in-up" fill="white" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1h-2z"/>
+            <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z"/>
+          </svg>
           </router-link>
         </div>
       </div>
     </b-navbar>
   </div>
 </template>
-
+// https://icons.getbootstrap.com/ or https://mdbootstrap.com/docs/vue/content/icons-list/
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
@@ -36,18 +45,6 @@ export default class TopHeader extends Vue {
   };//  get the property value from mother component, or by emitting the value. Currently just local variable
 
   searchBarInput = "";
-
-//   checkRoute(route: string) {
-//       if (route === "opgaver") {
-//           this.$router.push({ path: "/" });
-//       } else if (route === "login") {
-//           this.$router.push({ path: "/" });
-//       } else if (route === "profile") {
-//           this.$router.push({ path: "/" });
-//       } else {
-//           console.log("Routing error");
-//       }
-//   };
 }
 </script>
 
@@ -64,9 +61,9 @@ export default class TopHeader extends Vue {
   width: 90%;
 }
 .opret-arbejdsopgaver-button{
-    width: 10%;
 }
 .login-div {
+    padding-left: 40px;
 }
 .header {
   position: fixed; /* fixing the position takes it out of html flow - knows
