954992fb94804d716226a528cfbc2ef8919e756d
Alexander Stenbøg <ast@visiosign.com>
2020-12-09 13:16:46 +0100

    See more design + search onsubmit

--- spa/src/components/TopHeader.vue
+++ spa/src/components/TopHeader.vue
@@ -2,11 +2,12 @@
     <div id="rootDiv">
         <b-navbar type="dark" class="header">
             <b-navbar-brand class="brand" to="/">Workio</b-navbar-brand>
-            <b-form-input
+            <b-form class="search-bar" @submit.prevent="submit">
+                <b-form-input
                 placeholder="Søg"
-                class="search-bar"
                 v-model="searchBarInput"
                 ></b-form-input>
+            </b-form>
                     <b-button variant="light" class="ml-auto arbejdsopgave-btn" to="/arbejdsopgave" v-if="loggedIn">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -49,7 +50,7 @@ export default class TopHeader extends Vue {
         this.loggedIn = this.$store.state.loggedIn;
     }
 
-    @Watch("searchBarInput") f() {
+    submit() {
         if (this.searchBarInput !== "") {
             this.NewQuery("search", this.searchBarInput);
         } else {
