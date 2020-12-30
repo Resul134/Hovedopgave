922f6e2329d489ab1c47ea4a13f9939a5707baa6
Resul134 <43374840+Resul134@users.noreply.github.com>
2020-12-30 16:19:18 +0100

    Revert "Update SeeMore.vue"
    This reverts commit 45dba2127c541ca66371cff23781b08422d7adfa.

--- spa/src/views/SeeMore.vue
+++ spa/src/views/SeeMore.vue
@@ -87,6 +87,11 @@ export default class SeeMore extends Vue {
     description = "";
     region = "";
     status = "";
+    pageViews = null;
+    userID = null;
+    categoryID = null;
+    promoted = null;
+    promotedEnd: Date = new Date();
 
     user = {} as User;
     firstName = "";
@@ -115,12 +120,17 @@ export default class SeeMore extends Vue {
         } else {
             GetTaskById(this.$store.state.taskID).then(response => {
                 this.task = response.data;
+                this.userID = response.data.userID;
+                this.categoryID = response.data.categoryID;
                 this.region = response.data.region;
                 this.status = response.data.status;
                 this.date = response.data.date;
                 this.title = response.data.title;
                 this.price = response.data.price;
                 this.description = response.data.description;
+                this.pageViews = response.data.pageViews;
+                this.promoted = response.data.promoted;
+                this.promotedEnd = response.data.promotedEnd;
 
                 this.loadKommentarer();
                 GetBrugerById(this.task.userId).then(response => {
