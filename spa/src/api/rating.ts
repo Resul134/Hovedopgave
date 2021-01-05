c7c954f907bef6845bbf3ddd81a1edc5109f4aaf
Simon Nielsen <43374884+GretheSlushice@users.noreply.github.com>
2021-01-06 00:30:10 +0100

    Retry

--- spa/src/api/rating.ts
+++ spa/src/api/rating.ts
@@ -8,3 +8,17 @@ export function GetRatings() {
 export function GetRatingsByUserId(id: number) {
     return axios.get(`${apiEndPoint}/ratings/user/${id}`);
 }
+
+export function GetRatingByUserIdAndTaskId(userId: number, taskId: number) {
+    return axios.get(`${apiEndPoint}/ratings/user/${userId}/task/${taskId}`);
+}
+
+export function CreateRating(UserID: number, TaskID: number, Date: Date, Rating: number, Message: string) {
+    return axios.post(`${apiEndPoint}ratings`, {
+        UserID: UserID,
+        TaskID: TaskID,
+        Date: Date,
+        Rating: Rating,
+        Message: Message
+    });
+}
